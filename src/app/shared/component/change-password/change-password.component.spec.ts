1de8c114867af5f3fcb95fb5b2fb590d1fa89438
fmikko <francismikko.antonio@lgusuite.com>
2021-10-13 13:25:23 +0800

    added karma in change-password.component

--- src/app/shared/component/change-password/change-password.component.spec.ts
+++ src/app/shared/component/change-password/change-password.component.spec.ts
@@ -2,25 +2,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './../../material.module';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
 import { MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { of } from 'rxjs';
 
-describe('ChangePasswordComponent', () => {
+fdescribe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
+  let authService: AuthService
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangePasswordComponent],
-      imports:[MaterialModule,ReactiveFormsModule,HttpClientTestingModule,BrowserAnimationsModule],
-         providers: [{ provide: MatDialogRef, useValue: {} }]
+      declarations: [ChangePasswordComponent],
+      imports: [MaterialModule, ReactiveFormsModule, HttpClientTestingModule, BrowserAnimationsModule],
+      providers: [{ provide: MatDialogRef, useValue: {} }]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
+    authService = TestBed.inject(AuthService)
     fixture = TestBed.createComponent(ChangePasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -28,5 +32,40 @@ describe('ChangePasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('won\'t enable save password button if one of the field is empty or more', fakeAsync(() => {
+    component.passwordForm.setValue({
+
+      newPassword: '123qweasdzxc123',
+      passwordConfirm: '123qweasdzxc123',
+    });
+    spyOn(authService, 'register').and.returnValue(
+      of({
+        res: {
+          env: {
+            status: 'success',
+          },
+        },
+      })
+    );
+    component.save();
+    tick(200);
+    fixture.detectChanges();
+    expect(component.passwordForm.valid).toBeTrue()
+    expect(component.isSaving).toBeTrue();
+  }));
+
+  it('should not register if the new password and password confirm are not same', () => {
+    component.passwordForm.setValue({
+      newPassword: '123qweasdzxc123',
+      passwordConfirm: '123qweasdzxc123',
+    });
+
+    component.save();
+
+    expect(component.passwordForm.getRawValue().newPassword).toBe(
+      component.passwordForm.getRawValue().passwordConfirm
+    );
   });
 });
